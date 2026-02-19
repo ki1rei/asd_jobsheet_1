01508e2fdb28a9d820429a046bb470b4bd24845f
ki1rei <studyacctyara@gmail.com>
2026-02-19 20:05:14 +0700

    updated

--- Mahasiswa13.java
+++ Mahasiswa13.java
@@ -6,6 +6,16 @@ class Mahasiswa {
     String kelas;
     double ipk;
 
+    public Mahasiswa () {
+
+    }
+
+    public Mahasiswa (String nm, String nim, double ipk, String kls) {
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
     void tampilkanInformasi() {
         System.out.println("Nama: " + nama);
         System.out.println("NIM: " + nim);
--- MahasiswaMain.java
+++ MahasiswaMain.java
@@ -10,5 +10,9 @@ public class MahasiswaMain {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa mhs2 = new Mahasiswa("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
